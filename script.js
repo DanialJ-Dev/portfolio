96fece8104e45538fe6757441f3f1b39d4f47cc3
Danial <75368856+DanialJ-Dev@users.noreply.github.com>
2025-10-23 12:21:43 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
+const imagesToPreload = [
+  "./images/celestial-births.jpg",
+  "./images/quiz-app-js.jpg",
+  "./images/todo-app-js.jpg",
+  "./images/social-proof-section.jpg",
+  "./images/results-summary-component.jpg",
+  "./images/stats-preview-card.jpg",
+  "./images/nft-preview-card-component.jpg",
+  "./images/blog-preview-card.jpg",
+  "./images/profile-card-component.jpg",
+  "./images/order-summary.jpg",
+];
 
-    document.addEventListener('DOMContentLoaded', () => {
-    const hamburger = document.querySelector('.hamburger-menu');
-    const navList = document.querySelector('.nav-list');
+imagesToPreload.forEach((src) => {
+  const img = new Image();
+  img.src = src;
+});
 
-    hamburger.addEventListener('click', (e) => {
-        e.stopPropagation(); // جلوگیری از بسته شدن فوری منو
-        navList.classList.toggle('show');
-    });
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(".hamburger-menu");
+  const navList = document.querySelector(".nav-list");
 
-    navList.addEventListener('click', (e) => {
-        e.stopPropagation(); // جلوگیری از بسته شدن هنگام کلیک داخل منو
-    });
+  hamburger.addEventListener("click", (e) => {
+    e.stopPropagation(); // جلوگیری از بسته شدن فوری منو
+    navList.classList.toggle("show");
+  });
 
-    document.addEventListener('click', () => {
-        navList.classList.remove('show'); // بستن منو وقتی بیرون کلیک شد
-    });
+  navList.addEventListener("click", (e) => {
+    e.stopPropagation(); // جلوگیری از بسته شدن هنگام کلیک داخل منو
+  });
+
+  document.addEventListener("click", () => {
+    navList.classList.remove("show"); // بستن منو وقتی بیرون کلیک شد
+  });
 });
